326ca808576271f6e6347e6d3f1c498bbd3af99b
Saurav Desai <dsaurav16@gmail.com>
2021-10-24 16:12:11 +0530

    Added a Function to limit the random number generated between two integers

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function generatenumber() {
     var x = document.getElementById("number")
     x.innerHTML = Math.floor((Math.random() * 100) + 1);
   }
+
+  function getRandomInt(min, max) {
+    var min = 10;
+    var max =25;
+    var x = document.getElementById("number")
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    x.innerHTML = Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
